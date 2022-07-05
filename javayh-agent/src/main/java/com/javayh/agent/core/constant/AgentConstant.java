043823c5d2398d474c93692a11ff77bf47967ec2
yanghaiji <hai.ji.yang@ibm.com>
2022-07-05 17:31:07 +0800

    update util :new:

--- javayh-agent/src/main/java/com/javayh/agent/core/constant/AgentConstant.java
+++ javayh-agent/src/main/java/com/javayh/agent/core/constant/AgentConstant.java
@@ -16,4 +16,19 @@ public interface AgentConstant {
     String S_LINE= "/";
     String H_LINE = "-";
 
+    String[] HEADERS_TO_TRY = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+
 }
--- javayh-agent/src/main/java/com/javayh/agent/core/utils/IpAddressUtil.java
+++ javayh-agent/src/main/java/com/javayh/agent/core/utils/IpAddressUtil.java
@@ -1,43 +1,35 @@
 package com.javayh.agent.core.utils;
 
+import com.javayh.agent.core.constant.AgentConstant;
+
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * @author haiji
  */
 public class IpAddressUtil {
 
-    public static String getIpAddress(HttpServletRequest request ){
-        String ipAddress = request.getHeader("x-forwarded-for");
-        if((ipAddress == null) || (ipAddress.length() == 0) || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("Proxy-Client-IP");
-        }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getRemoteAddr();
-            if("127.0.0.1".equals(ipAddress) || "0:0:0:0:0:0:0:1".equals(ipAddress)){
-                //根据网卡取本机配置的IP
-                InetAddress inet=null;
-                try {
-                    inet = InetAddress.getLocalHost();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }
-                assert inet != null;
-                ipAddress= inet.getHostAddress();
+
+    /**
+     * 获取用户真实IP地址
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        String rip = request.getRemoteAddr();
+        for (String header : AgentConstant.HEADERS_TO_TRY) {
+            String ip = request.getHeader(header);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                rip = ip;
+                break;
             }
         }
-        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-        if(ipAddress!=null && ipAddress.length()>15){
-            if(ipAddress.indexOf(",")>0){
-                ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));
-            }
+        int pos = rip.indexOf(',');
+        if (pos >= 0) {
+            rip = rip.substring(0, pos);
         }
-        return ipAddress;
+        return rip;
     }
 
 }
