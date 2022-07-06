454500ca347ed95a4faeca063ff4d02421fed92e
yanghaiji <hai.ji.yang@ibm.com>
2022-07-06 14:46:59 +0800

    add feign :new:

---
+++ javayh-agent/src/main/java/com/javayh/agent/core/config/AgentFeignAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.javayh.agent.core.config;
+
+
+import com.javayh.agent.core.interceptor.AgentLogFeignInterceptor;
+import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author haiji
+ */
+@Slf4j
+@Configuration
+public class AgentFeignAutoConfiguration {
+
+
+    @Bean
+    @ConditionalOnMissingBean(AgentLogFeignInterceptor.class)
+    public RequestInterceptor feignRequestInterceptor() {
+        AgentLogFeignInterceptor interceptor = new AgentLogFeignInterceptor();
+        log.info("AgentLogFeignInterceptor [{}]", interceptor);
+        return interceptor;
+    }
+
+
+}
---
+++ javayh-agent/src/main/java/com/javayh/agent/core/interceptor/AgentLogFeignInterceptor.java
@@ -0,0 +1,47 @@
+package com.javayh.agent.core.interceptor;
+
+import com.javayh.agent.core.bean.LoggerCollector;
+import com.javayh.agent.core.constant.LoggerType;
+import com.javayh.agent.core.context.TraceContext;
+import com.javayh.agent.core.utils.IpAddressUtil;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2022-07-05
+ */
+@Slf4j
+public class AgentLogFeignInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate tmp) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+            String url = tmp.url();
+            String query = tmp.queryLine();
+            String body = Arrays.toString(tmp.body());
+            String method = tmp.method();
+            LoggerCollector collector = LoggerCollector.builder()
+                    .url(url).query(query).ip(IpAddressUtil.getIpAddress(request))
+                    .type(LoggerType.FEIGN.value()).method(method)
+                    .body(body)
+                    .createTime(new Date()).traceId(TraceContext.getTraceId()).build();
+            log.info("AgentLogFeign ==> {}", collector);
+        }
+    }
+}
