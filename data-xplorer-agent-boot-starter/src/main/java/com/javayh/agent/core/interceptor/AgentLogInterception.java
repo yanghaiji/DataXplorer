0606f9c15e4f72098bdd6126d24bc9002fbfc571
yanghaiji <hai.ji.yang@ibm.com>
2023-11-07 09:11:23 +0800

    实现url 过滤

--- data-xplorer-agent-boot-starter/src/main/java/com/javayh/agent/core/interceptor/AgentLogInterception.java
+++ data-xplorer-agent-boot-starter/src/main/java/com/javayh/agent/core/interceptor/AgentLogInterception.java
@@ -5,6 +5,7 @@ import com.google.protobuf.util.Timestamps;
 import com.javayh.agent.common.bean.proto.LoggerCollectorProto;
 import com.javayh.agent.common.bean.proto.MessageTypeProto;
 import com.javayh.agent.common.cache.AgentCacheQueue;
+import com.javayh.agent.common.configuration.DataXplorerProperties;
 import com.javayh.agent.common.constant.LoggerSourceType;
 import com.javayh.agent.common.constant.LoggerType;
 import com.javayh.agent.common.context.AppNamingContext;
@@ -22,6 +23,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -32,10 +34,13 @@ public class AgentLogInterception implements HandlerInterceptor {
 
     private final AppNamingContext appNamingContext;
     private final UserInfoRepository userInfoRepository;
+    private final DataXplorerProperties dataXplorerProperties;
 
-    public AgentLogInterception(AppNamingContext appNamingContext, UserInfoRepository userInfoRepository) {
+    public AgentLogInterception(AppNamingContext appNamingContext, UserInfoRepository userInfoRepository,
+                                DataXplorerProperties dataXplorerProperties) {
         this.appNamingContext = appNamingContext;
         this.userInfoRepository = userInfoRepository;
+        this.dataXplorerProperties = dataXplorerProperties;
     }
 
     private static final Logger log = LoggerFactory.getLogger(AgentLogInterception.class);
@@ -66,44 +71,48 @@ public class AgentLogInterception implements HandlerInterceptor {
         StopWatch stopWatch = STOP_WATCH_THREAD_LOCAL.get();
         stopWatch.stop();
         String url = request.getRequestURI();
-        String query = request.getQueryString();
-        String method = request.getMethod();
-        String traceId = request.getParameter("TraceId");
-        if (StringUtils.isEmpty(traceId)) {
-            traceId = request.getHeader("TraceId");
+        List<String> excludeUrl = dataXplorerProperties.getExcludeUrl();
+        if (!excludeUrl.contains(url)) {
+            String query = request.getQueryString();
+            String method = request.getMethod();
+            String traceId = request.getParameter("TraceId");
+            if (StringUtils.isEmpty(traceId)) {
+                traceId = request.getHeader("TraceId");
+            }
+            String user = userInfoRepository.queryUserName();
+            String ip = IpUtil.getIpAddr(request);
+            String body = BODY.get();
+            LoggerCollectorProto.LoggerCollector collector = LoggerCollectorProto.LoggerCollector.newBuilder()
+                    .setUrl(url)
+                    .setQuery(reset(query))
+                    .setBody(reset(body))
+                    .setIp(ip)
+                    .setAppName(appNamingContext.getAppNaming())
+                    .setActionTime(stopWatch.getTime())
+                    .setType(LoggerType.INTERCEPTOR.value())
+                    .setMethod(method)
+                    .setCreateTime(Timestamps.fromMillis(System.currentTimeMillis()))
+                    .setTraceId(StringUtils.isEmpty(traceId) ? TraceContext.getTraceId() : traceId)
+                    .setSourceType(LoggerSourceType.AUTOMATIC.value())
+                    .setMessageType(MessageTypeProto.MessageType.LOGGER_COLLECTOR)
+                    // TODO: 2023/9/19 根据实际的项目进行集成
+                    .setCreateBy(user)
+                    .build();
+            if (Objects.nonNull(ex)) {
+                collector.toBuilder().setErrorMsg(ExceptionUtils.getStackTrace(ex));
+            }
+            if (log.isDebugEnabled() && dataXplorerProperties.getShowLog()) {
+                log.debug("DataXplorer 拦截日志 : {}", collector);
+            }
+            if (log.isInfoEnabled() && dataXplorerProperties.getShowLog()) {
+                log.info("DataXplorer 拦截日志 : {}", collector);
+            }
+            STOP_WATCH_THREAD_LOCAL.remove();
+            BODY.remove();
+            TraceContext.remove();
+            AgentCacheQueue.MSG_CACHE_DE.offer(collector);
         }
-        String user = userInfoRepository.queryUserName();
-        String ip = IpUtil.getIpAddr(request);
-        String body = BODY.get();
-        LoggerCollectorProto.LoggerCollector collector = LoggerCollectorProto.LoggerCollector.newBuilder()
-                .setUrl(url)
-                .setQuery(reset(query))
-                .setBody(reset(body))
-                .setIp(ip)
-                .setAppName(appNamingContext.getAppNaming())
-                .setActionTime(stopWatch.getTime())
-                .setType(LoggerType.INTERCEPTOR.value())
-                .setMethod(method)
-                .setCreateTime(Timestamps.fromMillis(System.currentTimeMillis()))
-                .setTraceId(StringUtils.isEmpty(traceId) ? TraceContext.getTraceId() : traceId)
-                .setSourceType(LoggerSourceType.AUTOMATIC.value())
-                .setMessageType(MessageTypeProto.MessageType.LOGGER_COLLECTOR)
-                // TODO: 2023/9/19 根据实际的项目进行集成
-                .setCreateBy(user)
-                .build();
-        if (Objects.nonNull(ex)) {
-            collector.toBuilder().setErrorMsg(ExceptionUtils.getStackTrace(ex));
-        }
-        if (log.isDebugEnabled()) {
-            log.debug("agent 拦截日志 : {}", collector);
-        }
-        if (log.isInfoEnabled()) {
-            log.info("agent 拦截日志 : {}", collector);
-        }
-        STOP_WATCH_THREAD_LOCAL.remove();
-        BODY.remove();
-        TraceContext.remove();
-        AgentCacheQueue.MSG_CACHE_DE.offer(collector);
+
     }
 
     private String reset(String value) {
